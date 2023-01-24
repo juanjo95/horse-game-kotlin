4b8b1c322afe02610d419c92c3d7d9aa4b5e10bc
Juan J Salgado <zallagox95@hotmail.com>
2023-01-24 06:29:24 -0500

    feat 🚀: Redimensionar de elementos y quitar ActionBar

--- app/src/main/java/com/androidavanzadotutorial/horsegame/MainActivity.kt
+++ app/src/main/java/com/androidavanzadotutorial/horsegame/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.androidavanzadotutorial.horsegame
 
+import android.graphics.Point
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TableRow
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initScreenGame()
     }
+
+    private fun initScreenGame(){
+        setSizeBoard()
+        hide_message()
+    }
+
+    private fun setSizeBoard(){
+        var iv:ImageView
+
+        val display = windowManager.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+        val width = size.x
+
+        var width_dp = (width / getResources().getDisplayMetrics().density)
+        var lateralMarginDP = 0
+        val width_cell = (width_dp - lateralMarginDP) / 8
+        val height_cell = width_cell
+
+        for(i in 0..7){
+            for(j in 0..7){
+                iv = findViewById(resources.getIdentifier("c$i$j","id",packageName))
+                var height = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,height_cell,getResources().getDisplayMetrics()).toInt()
+                var width = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,width_cell,getResources().getDisplayMetrics()).toInt()
+                iv.setLayoutParams(TableRow.LayoutParams(width,height))
+            }
+        }
+    }
+
+    private fun hide_message(){
+        var lyMessage:LinearLayout = findViewById(R.id.lyMessage)
+        lyMessage.visibility = View.INVISIBLE
+    }
+
 }

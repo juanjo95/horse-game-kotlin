e51ccc4aac25c54cf575b52499c729cc14b4dbe3
Juan J Salgado <zallagox95@hotmail.com>
2023-01-25 06:36:44 -0500

    feat 🚀: Limitaciones de movimientos del usuario

--- app/src/main/java/com/androidavanzadotutorial/horsegame/MainActivity.kt
+++ app/src/main/java/com/androidavanzadotutorial/horsegame/MainActivity.kt
@@ -22,9 +22,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initScreenGame()
-        resetBoard()
-        setFirstPosition()
+        this.initScreenGame()
+        this.resetBoard()
+        this.setFirstPosition()
     }
 
     private fun initScreenGame(){
@@ -106,5 +106,32 @@ class MainActivity : AppCompatActivity() {
             intArrayOf(0,0,0,0,0,0,0,0),
             intArrayOf(0,0,0,0,0,0,0,0)
         )
+    }
+
+    fun checkCellClicked(v:View){
+        var name = v.tag.toString()
+        var x = name.subSequence(1,2).toString().toInt()
+        var y = name.subSequence(2,3).toString().toInt()
+
+        this.checkCell(x,y)
+    }
+
+    private fun checkCell(x:Int, y:Int){
+        var dif_x = x - this.cellSelected_x
+        var dif_y = y - this.cellSelected_y
+
+        var checkTrue = false
+        if(dif_x == 1 && dif_y == 2)   checkTrue = true // right - top long
+        if(dif_x == 1 && dif_y == -2)  checkTrue = true // right - bottom long
+        if(dif_x == 2 && dif_y == 1)   checkTrue = true // right long - top
+        if(dif_x == 2 && dif_y == -1)  checkTrue = true // right long - bottom
+        if(dif_x == -1 && dif_y == 2)  checkTrue = true // left - top long
+        if(dif_x == -1 && dif_y == -2) checkTrue = true // left - bottom long
+        if(dif_x == -2 && dif_y == 1)  checkTrue = true // left long - top
+        if(dif_x == -2 && dif_y == -1) checkTrue = true // left long - bottom
+
+        if(this.board[x][y] == 1) checkTrue = false
+
+        if(checkTrue) this.selectCell(x,y)
     }
 }
